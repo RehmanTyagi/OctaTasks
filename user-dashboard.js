1bc17c67dc98ba5babf3b251ade57781ff2f71e2
RehmanTyagi <irontyagi4@gmail.com>
2022-02-02 13:30:21 +0530

    dashboard done

--- user-dashboard.js
+++ user-dashboard.js
@@ -1,20 +1,27 @@
 // navbar started from here
 
 function openSupport() {
-  document.getElementById("user-support-area").style.display = "unset";
-  document.getElementById("user-support-area").style.display = "flex";
+    document.getElementById("user-support-area").style.display = "unset";
+    document.getElementById("user-support-area").style.display = "flex";
 }
+
 function openPanel() {
-  document.getElementById("user-panel-area").style.display = "unset";
-  document.getElementById("user-panel-area").style.display = "flex";
+    document.getElementById("user-panel-area").style.display = "unset";
+    document.getElementById("user-panel-area").style.display = "flex";
 }
+
 function pageprogressLoader() {
-  document.getElementById("page-loader").style.display = "unset";
-  document.getElementById("page-loader").style.position = "fixed";
+    document.getElementById("page-loader").style.display = "unset";
+    document.getElementById("page-loader").style.position = "fixed";
 }
 
 function runSpinner() {
-  document.getElementById("spinner").style.display = "unset";
-  document.getElementById("spinner").style.zIndex = "10";
-  document.getElementById("dashboard-body").style.opacity = "0.3";
+    document.getElementById("spinner").style.display = "unset";
+    document.getElementById("spinner").style.zIndex = "10";
+    document.getElementById("dashboard-body").style.opacity = "0.1";
+}
+
+
+function workEngine() {
+    window.location.replace('');
 }
