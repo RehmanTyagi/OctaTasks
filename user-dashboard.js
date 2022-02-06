044e15444b8a420daa9a2ca7f05d35dacfe6b7b7
RehmanTyagi <81377973+RehmanTyagi@users.noreply.github.com>
2022-02-06 23:24:10 +0530

    inbox has completed

--- user-dashboard.js
+++ user-dashboard.js
@@ -1,32 +1,44 @@
 // navbar started from here
 
 function openSupport() {
-    document.getElementById("user-support-area").style.display = "unset";
-    document.getElementById("user-support-area").style.display = "flex";
+  document.getElementById("user-support-area").style.display = "unset";
+  document.getElementById("user-support-area").style.display = "flex";
 }
 
 function openPanel() {
-    document.getElementById("user-panel-area").style.display = "unset";
-    document.getElementById("user-panel-area").style.display = "flex";
+  document.getElementById("user-panel-area").style.display = "unset";
+  document.getElementById("user-panel-area").style.display = "flex";
 }
 
 function pageprogressLoader() {
-    document.getElementById("page-loader").style.display = "unset";
-    document.getElementById("page-loader").style.position = "fixed";
+  document.getElementById("page-loader").style.display = "unset";
+  document.getElementById("page-loader").style.position = "fixed";
 }
 
 function runSpinner() {
-    document.getElementById("spinner").style.display = "unset";
-    document.getElementById("spinner").style.zIndex = "10";
-    document.getElementById("dashboard-body").style.opacity = "0.06";
+  document.getElementById("spinner").style.display = "unset";
+  document.getElementById("spinner").style.zIndex = "10";
+  document.getElementById("dashboard-body").style.opacity = "0.06";
 }
 
 function refengine() {
-    document.getElementById("referral-container").style.display = "unset";
-    document.getElementById("referral-container").style.zIndex = "11";
-    document.getElementById("referral-container").style.display = "flex";
+  document.getElementById("referral-container").style.display = "unset";
+  document.getElementById("referral-container").style.zIndex = "11";
+  document.getElementById("referral-container").style.display = "flex";
 }
 
 function refengineoff() {
-    document.getElementById("referral-container").style.display = "none";
+  document.getElementById("referral-container").style.display = "none";
 }
+
+if (notificationShow == 2) {
+  function notificationShow() {
+    document.querySelector(".notificaton-area").style.display = "unset";
+  }
+} else {
+  function notificationShow() {
+    document.querySelector(".notificaton-area").style.display = "unset";
+  }
+}
+
+document.querySelector(".fa-bell").addEventListener("click", notificationShow);
